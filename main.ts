6e0d83e2375f43315ac26d01cd1e3d411273e6f9
jli0714 <86691162+jli0714@users.noreply.github.com>
2021-07-08 12:16:55 +1200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . . # . .
+        # # # # #
+        . # # # .
+        . . # . .
+        . # # # .
+        `)
+    basic.pause(50)
+    basic.showLeds(`
+        . . # . .
+        # # # # #
+        # # # # #
+        # # . # #
+        . # . # .
+        `)
+    basic.showString("Iron Golem Boi")
+})
